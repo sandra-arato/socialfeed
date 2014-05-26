44edb6a42c5706b72b105660db81da68454fc0e7
Sandra Szenti <alexandraszenti@gmail.com>
2014-05-26 16:50:05 +0100

    ejs update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 (function($){
 
+	function getPosts () {
+		FB.api(
+			"/v2.0/me/feed/",
+			function (response) {
+				if (response && !response.error) {
+					/* handle the result */
+					console.log(response);
+					return response;
+				}
+			});
+	}
 	window.fbAsyncInit = function() {
 		FB.init({
 		appId	: '324540777693543',
@@ -12,20 +23,9 @@
 		if (syncInitFB) {
 			FB.login(function(response) {
 				console.log("fb ready");
-				FB.api(
-				"/v2.0/me/feed/",
-				function (response) {
-					if (response && !response.error) {
-						/* handle the result */
-						console.log(response);
-						if (EJS) {
-							var html = new EJS({url: '../js/fb.ejs'}).render(response);
-							$(html).appendTo($("div.container"));
-						};
-					}
-				});
 			}, {scope: "read_stream"});
-			
+			var html = new EJS({url: '../js/fb.ejs'}).render(getPosts());
+			$(html).appendTo($("div.container"));
 		};
 	};
 })(jQuery);
